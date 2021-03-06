e94ffea220a25bc8c93aecebfc444a9e6468bcfb
Cat <ormallen@gmail.com>
2021-03-06 22:08:43 +0300

    init: basic template

--- src/js/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-function App() {
-  return <div>Travel app is running</div>;
-}
-
-ReactDom.render(<App />, document.querySelector('#main'));

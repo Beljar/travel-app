3db8ad178c31099e9e248754a6cd40fc50253f05
SuzyGRBT <lady94@inbox.ru>
2021-03-11 22:14:39 +0300

    feat: add cards for countries, add style

--- src/js/Components/MainPageComponent.jsx
+++ src/js/Components/MainPageComponent.jsx
@@ -4,6 +4,7 @@ import { Layout, Input, Select } from 'antd'
 import ghImg from '../assets/GitHub-Mark.png'
 import logo from '../assets/rs_school_js.svg';
 import mainLogo from '../assets/travel.svg';
+import countryList from '../assets/countryList.jsx'
 
 const { Header, Content, Footer } = Layout; 
 const { Search } = Input;
@@ -15,12 +16,23 @@ const MainPage = () => {
         console.log(`selected ${value}`);
     }
 
+    const countryCards = countryList.map((image, index) => {
+        return (
+            <div className="countryCard" key={index} onClick={() => console.log('clicked')}>
+                <img className="countryImage" src={"src/js/assets/mainPageCountry/" + image.url} width="300px" height="200px" />
+                <div className="cardHover">
+                    <div className="text">{image.country}</div>
+                </div>
+            </div>
+        )
+    })
+
     return (
         <Layout>
             <Header className="header">
                 <Search className="search-input" placeholder="Страна или столица" allowClear onSearch={onSearch} enterButton />
-                    <a href="#" target="_blank" rel="noreferrer" style={{ margin: 0 }}>
-                        <img src={mainLogo} alt="mainLogo" height="50px" width="50px" />
+                    <a href="#" className="mainLogoLink" target="_blank" rel="noreferrer" style={{ margin: 0 }}>
+                        <img className="mainLogoImage" src={mainLogo} alt="mainLogo" height="50px" width="50px" />
                     </a>
                 <Select defaultValue="rus" style={{ width: 120, margin: '1%' }} onChange={handleChange}>
                     <Option value="rus">Русский</Option>
@@ -28,7 +40,11 @@ const MainPage = () => {
                     <Option value="blr">Белорусский</Option>
                 </Select>
             </Header>
-            <Content className="site-layout"></Content>
+            <Content className="site-layout">
+                <div className="countryCards">
+                    {countryCards}
+                </div>
+            </Content>
             <Footer className="footer">
                 <a href="https://github.com/SuzyGRBT" target="_blank" rel="noreferrer" className="footer-item">
                     <img src={ghImg} alt="Suzanna" height="30px" width="30px" style={{ marginRight: 5 }}  />
---
+++ src/js/assets/countryList.jsx
@@ -0,0 +1,43 @@
+const countryList = [
+    {
+        url: "britain.jpg",
+        country: "Great Britain"
+    },
+
+    {
+        url: "canada.jpg",
+        country: "Canada"
+    },
+
+    {
+        url: "france.jpg",
+        country: "France"
+    },
+
+    {
+        url: "germany.jpg",
+        country: "Germany"
+    },
+
+    {
+        url: "italy.jpg",
+        country: "Italy"
+    },
+
+    {   
+        url: "norway.jpg",
+        country: "Norway"
+    },
+
+    {   
+        url: "spain.jpg",
+        country: "Spain"
+    },
+
+    {
+        url: "usa.jpg",
+        country: "United States of America"
+    }
+]
+
+export default countryList;

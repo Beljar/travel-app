102231d65eb84ec75a100f5c1923a218a40fb1ee
Cat <ormallen@gmail.com>
2021-03-17 03:06:35 +0300

    initial commit

--- src/js/Components/video-player/video-player.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-
-export default function VideoPlayer({ url, width, style }) {
-  return <ReactPlayer style={style} width={width} url={url} controls />;
-}
--- src/js/utils/capitalize.js
+++
@@ -1,3 +0,0 @@
-const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
-
-export default capitalize;
